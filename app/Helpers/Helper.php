fe3d415440f2e4d3242fe90c7c3865e401f63185
mazeko <Eko.Purnomo@sera.astra.co.id>
2024-01-09 14:54:26 +0700

    enhance helper

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -3,14 +3,15 @@
 namespace App\Helpers;
 
 class Helper {
-    public static function generateID($prevID)
+    public static function generateID($prevID, $length)
     {;
         preg_match('/([A-Z]+)-(\d+)/', $prevID, $matches);
         $prefix = $matches[1];
         $number = intval($matches[2]);
         $number++;
 
-        $newNumber = sprintf('%02d', $number);
+        $value = "%0".$length."d";
+        $newNumber = sprintf($value, $number);
         $newID = $prefix . '-' . $newNumber;
         
         return $newID;
